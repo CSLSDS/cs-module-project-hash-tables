8a21dcfea4ba4ea80ac75804b0912fa6af0265b1
cslsds <lambdadatasciencelearning@gmail.com>
2020-08-06 13:39:05 -0400

    clarifying sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -12,24 +12,34 @@ def f(x):
     return x * 4 + 6
 
 def sumdiff(q):
-    summed = {}
-    diffed = {}
-    for x in q:
-        for y in q:
-            comb = f(x) + f(y)
-            diff = f(x) - f(y)
-            if comb not in summed:
-                summed[comb] = {(x,y)}
+    # first we'll need a database of possible answers to compare
+    sumcache = {}
+    diffcache = {}
+    for x in q: # both seem necessary
+        for y in q: # to compare all possible combinations
+            added = f(x) + f(y)
+            subbed = f(x) - f(y)
+            # if new answer:
+            if added not in sumcache:
+                # initialize w possible key to list of keys per answer
+                sumcache[added] = {(x,y)}
             else:
-                summed[comb].add((x,y))
-            if diff not in diffed:
-                diffed[diff] = {(x,y)}
+                sumcache[added].add((x,y))
+            if subbed not in diffcache:
+                diffcache[subbed] = {(x,y)}
             else:
-                diffed[diff].add((x,y))
-    for val in summed:
-        if val in diffed:
-            for sumpair in summed[val]:
-                for diffpair in diffed[val]:
-                    print(f'Sum of f(n) of {sumpair} == diff of f(n) of {diffpair}.')
+                diffcache[subbed].add((x,y))
+    ''' now, for assembly! '''
+    # check each answer in cache of sums
+    for result in sumcache:
+        # and if it's matched in diff
+        if result in diffcache:
+            for sumpair in sumcache[result]:
+                for diffpair in diffcache[result]:
+                    print(f'f({sumpair[0]}) + f({sumpair[1]}) = \
+f({diffpair[0]} - f({diffpair[1]})    {f(sumpair[0])} + \
+{f(sumpair[1])} = {f(diffpair[0])} - {f(diffpair[1])}')
 
 print(sumdiff(q))
+
+''' but i can't tell why it's printing None at the end XD '''
